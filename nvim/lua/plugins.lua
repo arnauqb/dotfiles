89e6eb50074936b29e5620b1596821677a0b13de
Arnau Quera-Bofarull <arnauq@protonmail.com>
2023-05-10 16:28:33 +0100

    nvim config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,54 +1,25 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
--- 
-return require('packer').startup(function()
-
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-
-  -- Language servers
-  use {
-    'neovim/nvim-lspconfig',
-    'williamboman/nvim-lsp-installer',
-	}
-
-  -- julia
-  use 'JuliaEditorSupport/julia-vim'
-
-  -- fuzzy seaches
-  use {
-  'nvim-telescope/telescope.nvim',
-  requires = { {'nvim-lua/plenary.nvim'} },
+return {
+    {"nvim-treesitter/nvim-treesitter"},
+    {"neovim/nvim-lspconfig"},
+	{"kien/ctrlp.vim"},
+	{"averms/black-nvim"},
+	{"jpalardy/vim-slime"},
+	{"vim-test/vim-test"},
+	{"github/copilot.vim"},
+    {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+	{
+		"AstroNvim/astrotheme",
+		config = true,
+	},
+	{"preservim/vimux"},
+    {"christoomey/vim-tmux-navigator"},
+    {
+    'crispgm/nvim-tabline',
+    dependencies = { 'nvim-tree/nvim-web-devicons' }, -- optional
+    config = true,
+    },
+    {"rebelot/heirline.nvim"},
 }
-  use 'BurntSushi/ripgrep'
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
-
-  -- treesitter
-  use 'nvim-treesitter/nvim-treesitter'
-
-  -- tmux
-  use 'christoomey/vim-tmux-navigator'
-  use 'benmills/vimux'
-
-  -- colorschemes
-  use 'srcery-colors/srcery-vim'
-
-  -- git
-  use 'tpope/vim-fugitive'
-
-  -- yaml
-  use 'stephpy/vim-yaml'
-
-  -- snippets
-  use 'jpalardy/vim-slime'
-
-  -- formatters
-  use 'psf/black'
-  use 'kdheepak/JuliaFormatter.vim'
-
-  -- testing
-  use 'vim-test/vim-test'
-
-  -- latex 
-  use 'lervag/vimtex'
-
-end)
